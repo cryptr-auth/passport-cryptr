82de9d3da1be04d6e1a46ed868c271d1e5978467
Thibaud RENAUX <thibaud.renaux@gmail.com>
2022-08-05 15:35:25 +0200

    [Release] v1.0.0 - Support of SSO Gateway - Replace issuer in favor to base_url
    issuer is now build upon base_url and tnt
    
    ---
    
    Squashed commit of the following:
    
    commit 73f0da14bbbb992f0b7019912f99d11f613ca137
    Author: Thibaud RENAUX <thibaud.renaux@gmail.com>
    Date:   Fri Aug 5 15:31:56 2022 +0200
    
        quick change keywords
    
    commit 2804977fb7ea54f0ba58c37329d3f1cdb63c9b60
    Author: Thibaud RENAUX <thibaud.renaux@gmail.com>
    Date:   Fri Aug 5 15:18:15 2022 +0200
    
        improve readme with passport deps
    
    commit 00475a3b7efa329563e95f259020171fdfb9cfdc
    Author: Thibaud RENAUX <thibaud.renaux@gmail.com>
    Date:   Fri Aug 5 15:15:54 2022 +0200
    
        update package links
    
    commit 1e95ac4403de712626a52d302bbcbd075aa747d9
    Author: Thibaud RENAUX <thibaud.renaux@gmail.com>
    Date:   Fri Aug 5 15:15:10 2022 +0200
    
        remove custom publish npm registry
    
    commit 8ec99aed1796026c231610b274b6d370f47c7cc6
    Author: Thibaud RENAUX <thibaud.renaux@gmail.com>
    Date:   Fri Aug 5 15:14:10 2022 +0200
    
        update package keywords
    
    commit 6a3c1a46b30b9acb241234ddc4d1bfe09b546c16
    Author: Thibaud RENAUX <thibaud.renaux@gmail.com>
    Date:   Fri Aug 5 15:13:11 2022 +0200
    
        update version to 1.0.0
    
    commit 2038d459e405e682fd325dc5c84459af1887d105
    Author: Thibaud RENAUX <thibaud.renaux@gmail.com>
    Date:   Fri Aug 5 15:10:07 2022 +0200
    
        Feature/gateway support (#3)
    
        * update tests
    
        * change 'var' occurences to appropriate const or let
    
        * switch strategy to class
    
        * add base url to config
    
        * base of verification issuer using tnt claim
    
        * clean up code
    
        * fix deps
    
        * split constructor to reduce complexity
    
        * reduce complexity of authenticate
    
        * add coverage script
    
        * add testing for _innerVerified
    
        * cover most all code
    
        * ignore env files
    
        * improve dev test
    
        * cleanup and pretier config checking
    
        * adapt testing
    
        * update Readme
    
    commit ca12a308276a71fa198e68df553a2e76de17c2bd
    Merge: f3afa6a 643dd4e
    Author: Thibaud RENAUX <thibaud.renaux@gmail.com>
    Date:   Fri Jan 29 15:12:08 2021 +0100
    
        [Release] v0.1.0
    
        Merge branch 'release/v0.1.0' into develop

--- test/strategy.test.js
+++ test/strategy.test.js
@@ -1,14 +1,17 @@
-var Strategy = require("../lib/strategy");
+let Strategy = require("../lib/strategy");
 
-var validCryptrConfig = {
+let validCryptrConfig = {
+  base_url: "http://localhost:4000",
   audiences: ["http://localhost:3001"],
-  issuer: "http://localhost:4000",
   tenants: ["cryptr-cryptr"],
 };
 
+let otherTenantToken =
+  "eyJhbGciOiJSUzI1NiIsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6NDAwMC90L21pc2FwcmV0Iiwia2lkIjoiYWIwYWZmZDgtZDM0Zi00YjMwLWIwNGEtYzMzN2E2ZjNmNjBjIiwidHlwIjoiSldUIn0.eyJhcHBsaWNhdGlvbl9tZXRhZGF0YSI6e30sImF1ZCI6Imh0dHA6Ly9sb2NhbGhvc3Q6NDIwMCIsImNpZCI6ImY0MDdjYWZkLWI1OGEtNDcyZi1iODU3LTQ3NWE4NjNiNjliNiIsImRicyI6InNhbmRib3giLCJlbWFpbCI6ImphbmUuZG9lQGNyeXB0ci5jbyIsImV4cCI6MTY5MTE2NTE3NiwiaWF0IjoxNjU5NjI5MTc2LCJpcHMiOiJjcnlwdHIiLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjQwMDAvdC9taXNhcHJldCIsImp0aSI6ImE0MmYzZTQ4LTFlOWMtNDA0ZC1hZWE0LWNjM2ZjZmViNjRkYyIsImp0dCI6ImFjY2VzcyIsInNjaSI6bnVsbCwic2NwIjpudWxsLCJzdWIiOiI1MzRkY2JlMC1jODhkLTQ0MWQtODRjZC0xZGFhOGRiYzgzOTMiLCJ0bnQiOiJtaXNhcHJldCIsInZlciI6MX0.avoeKsVAv-7zOMsbOHg_8G9h9Mp5Pb4F74ywVWmoAyS2wz8EO3acdlrMgzqpZadNK6ts9RHawonjMAciOABS5HbRl-xBSytRDI2R2t_aHxOmkjKaoowePc4N1KQHTY7iZjAKLG_d7GA6vFvqq1Jf-ARWBhJ-rCTJYGmhClbD5ADxfpZXWzYq0vWDlu7Q8IFuSe-pfUqGec_TjklXMoTtHtkYxfOy0NNoyF3nn63movBi-CevCQmslkl2TR4IYQ9jpXFftfiF4W7Y5rYBbMzRK442nz6qs2k9hpO_0yWmGBI1tsThnVd-kl5ODwcwHojXazjxQINb_qNPhpxPDrbnJmPk4T7JRTV5CxDM_XWtXq4lVvem2LPjb387SP_EY0_udR7UklEDJ9mUwLR6uGxxr0nYn6EXy-JAp4dyHNvCVrMwgtM_47GASlk7-8tsmLAGUGhafhDEhImBMBwBxbfUWVlRpj3IC_9iv3U1oH3Rj4v1wSY_7-upJnHPQH1CrsXnYn-tREGqN8OIc8OZPAvdxY9WXMnNxZBCoMXFQ_LawXZrcZ0pbIfCu-Hrl9IxhtO4o9bzDqDvrY-MzioF_rIYAL-gysycr7Ib62hVkWmamRE5dVcnj3n3yUvfcVeHQugk5Ur8ZOoMSD65EXcPTv-whBdCpcQgd5E0V2ruzh9MEPs"
+
 describe('Cryptr Strategy - with valid cryptrconfig', () => {
   delete process.env.CRYPTR_TEST_MODE;
-  var validStrategy = new Strategy(
+  let validStrategy = new Strategy(
     {
       cryptrConfig: validCryptrConfig
     },
@@ -85,6 +88,7 @@ describe('Cryptr Strategy - with wrong Config', () => {
 
 describe('Cryptr Strategy - with empty Config', () => {
   delete process.env.CRYPTR_TEST_MODE;
+  delete process.env.CRYPTR_BASE_URL;
   delete process.env.CRYPTR_AUDIENCES;
   delete process.env.CRYPTR_TENANTS;
   delete process.env.CRYPTR_ISSUER;
@@ -107,18 +111,14 @@ describe('Cryptr Strategy - with empty Config', () => {
 describe("Cryptr Strategy - with valid cryptr config env vars", () => {
   beforeEach(() => {
     process.env = Object.assign(process.env, {
+      CRYPTR_BASE_URL: validCryptrConfig.base_url,
       CRYPTR_AUDIENCES: validCryptrConfig.audiences,
-      CRYPTR_ISSUER: validCryptrConfig.issuer,
       CRYPTR_TENANTS: validCryptrConfig.tenants,
     });
   });
 
   it("should have set CRYPTR_AUDIENCES", () => {
     expect(process.env.CRYPTR_AUDIENCES.split(',')).toEqual(validCryptrConfig.audiences);
-  });
-
-  it("should have set CRYPTR_ISSUER", () => {
-    expect(process.env.CRYPTR_ISSUER).toMatch(validCryptrConfig.issuer);
   });
 
   it("should have proper env cryptrConfig", () => {
@@ -149,10 +149,6 @@ describe("Cryptr Strategy - with valid env vars", () => {
     expect(process.env.CRYPTR_AUDIENCES.split(',')).toEqual(validCryptrConfig.audiences);
   });
 
-  it("should have set CRYPTR_ISSUER", () => {
-    expect(process.env.CRYPTR_ISSUER).toMatch(validCryptrConfig.issuer);
-  });
-
   it("should have proper env cryptrConfig", () => {
     var validStrategy = new Strategy(
       {},
@@ -174,7 +170,6 @@ describe('Cryptr Strategy - option as verify', () => {
   beforeEach(() => {
     process.env = Object.assign(process.env, {
       CRYPTR_AUDIENCES: validCryptrConfig.audiences,
-      CRYPTR_ISSUER: validCryptrConfig.issuer,
       NODE_ENV: 'development'
     });
   });
@@ -185,5 +180,58 @@ describe('Cryptr Strategy - option as verify', () => {
     var strategy = new Strategy(verify)
 
     expect(strategy._verify).toEqual(verify)
+  })
+})
+
+describe('Cryptr Strategy - _innerVerified', () => {
+   beforeEach(() => {
+    process.env = Object.assign(process.env, {
+      CRYPTR_BASE_URL: validCryptrConfig.base_url,
+      CRYPTR_AUDIENCES: validCryptrConfig.audiences,
+      CRYPTR_ISSUER: validCryptrConfig.issuer,
+      CRYPTR_TENANTS: validCryptrConfig.tenants,
+      NODE_ENV: 'development'
+    });
+  });
+
+  var verify = function (accessToken, idToken, profile, done) {}
+
+  it('should return error if error present', () => {
+    let strategy = new Strategy(verify);
+    const errorMsg = "Some error occured";
+    strategy.error = (err) => err;
+    const innerVerification = strategy._innerVerified(strategy, errorMsg, null, null)
+    expect(innerVerification).toEqual(errorMsg);
+  })
+ 
+  it('should return resource_owner and info if success', () => {
+    let strategy = new Strategy(verify);
+    const resource_owner = "jane.doe@cryptr.co";
+    const info = "Some info";
+    const successFn = jest.fn();
+    strategy.success = successFn;
+    strategy._innerVerified(strategy, null, resource_owner, info)
+    expect(successFn).toHaveBeenCalledWith(resource_owner, info);
+  })
+})
+
+describe('Cryptr Strategy - _verifyToken', () => {
+  beforeEach(() => {
+    process.env = Object.assign(process.env, {
+      CRYPTR_BASE_URL: validCryptrConfig.base_url,
+      CRYPTR_AUDIENCES: validCryptrConfig.audiences,
+      CRYPTR_ISSUER: validCryptrConfig.issuer,
+      CRYPTR_TENANTS: validCryptrConfig.tenants,
+      NODE_ENV: 'development'
+    });
+  });
+
+  let verify = function (accessToken, idToken, profile, done) {}
+
+  it('should crash if undefined token', () => {
+    let strategy = new Strategy(verify);
+    strategy.fail = (err) => err;
+    const res = strategy._verifyToken(undefined, strategy, null, null)
+    expect(res.toString()).toEqual("TypeError: Cannot read property 'split' of undefined");
   })
 })
